07dfbcf2d4ef2e5cc5cfdb8f7b9301c49c3dfe09
Ben Hicken <hickenb@sheridancollege.ca>
2025-02-13 09:58:00 -0500

    Implemented all of the needed code to switch out the visuals during the scene. Next Step: Implement the UI!

--- Assets/Scripts/BarrelCursor.cs
+++ Assets/Scripts/BarrelCursor.cs
@@ -9,6 +9,10 @@ public class BarrelCursor : MonoBehaviour
     public GameObject shell;
     public float bulletSpeed = 2;
     public float travelDistance = 5;
+    public Sprite[] shotAppearance = new Sprite[7];
+    public int spriteSelected = 0;
+    public AudioSource shotPlayer;
+    public AudioClip shotSFX;
 
     void Update()
     {
@@ -19,11 +23,13 @@ public class BarrelCursor : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
+            shotPlayer.PlayOneShot(shotSFX);
             GameObject tempShot = shell;
             shell.transform.position = transform.position;
             shell.transform.up = cursorPos;
             shell.GetComponent<ShotCode>().travelDistance = travelDistance;
             shell.GetComponent<ShotCode>().bulletSpeed = bulletSpeed;
+            shell.GetComponentInChildren<SpriteRenderer>().sprite = shotAppearance[spriteSelected];
             Instantiate(tempShot);
         }
     }
--- Assets/Scripts/TankPlayer.cs
+++ Assets/Scripts/TankPlayer.cs
@@ -5,10 +5,14 @@ using UnityEngine.Animations;
 
 public class TankPlayer : MonoBehaviour
 {
+    public Sprite[] tankAppearance = new Sprite[4];
+    public SpriteRenderer tankRenderer;
     public float speed;
+    public int spriteSelected = 0;
 
     void Update()
     {
+        tankRenderer.sprite = tankAppearance[spriteSelected];
         Vector3 velocity = new Vector3();
         velocity.x = Input.GetAxis("Horizontal") * speed;
         velocity = transform.position + velocity * Time.deltaTime;
