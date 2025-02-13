6e1efebd2553533d2715688f324e52984015a3fa
Ben Hicken <hickenb@sheridancollege.ca>
2025-02-13 10:34:29 -0500

    Final Commit. Had to alter the part of my plan that involved directly editing the variables from the slider component and changed some variables to floats to have them functino better, but tank is now finished!

--- Assets/Scripts/BarrelCursor.cs
+++ Assets/Scripts/BarrelCursor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Experimental.AI;
+using UnityEngine.UI;
 
 public class BarrelCursor : MonoBehaviour
 {
@@ -10,9 +11,13 @@ public class BarrelCursor : MonoBehaviour
     public float bulletSpeed = 2;
     public float travelDistance = 5;
     public Sprite[] shotAppearance = new Sprite[7];
-    public int spriteSelected = 0;
+    public float spriteSelected = 0;
     public AudioSource shotPlayer;
     public AudioClip shotSFX;
+
+    public GameObject BSpeed;
+    public GameObject BDistance;
+    public GameObject BSprite;
 
     void Update()
     {
@@ -29,8 +34,12 @@ public class BarrelCursor : MonoBehaviour
             shell.transform.up = cursorPos;
             shell.GetComponent<ShotCode>().travelDistance = travelDistance;
             shell.GetComponent<ShotCode>().bulletSpeed = bulletSpeed;
-            shell.GetComponentInChildren<SpriteRenderer>().sprite = shotAppearance[spriteSelected];
+            shell.GetComponentInChildren<SpriteRenderer>().sprite = shotAppearance[(int)spriteSelected];
             Instantiate(tempShot);
         }
+
+        bulletSpeed = BSpeed.GetComponent<Slider>().value;
+        spriteSelected = BSprite.GetComponent<Slider>().value;
+        travelDistance = BDistance.GetComponent<Slider>().value;
     }
 }
--- Assets/Scripts/TankPlayer.cs
+++ Assets/Scripts/TankPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Animations;
 
 public class TankPlayer : MonoBehaviour
@@ -8,14 +9,28 @@ public class TankPlayer : MonoBehaviour
     public Sprite[] tankAppearance = new Sprite[4];
     public SpriteRenderer tankRenderer;
     public float speed;
-    public int spriteSelected = 0;
+    public float spriteSelected = 0;
+
+    public GameObject TSprite;
+    public GameObject TSpeed;
 
     void Update()
     {
-        tankRenderer.sprite = tankAppearance[spriteSelected];
+        spriteSelected = TSprite.GetComponent<Slider>().value;
+        speed = TSpeed.GetComponent<Slider>().value;
+
+        tankRenderer.sprite = tankAppearance[(int)spriteSelected];
         Vector3 velocity = new Vector3();
         velocity.x = Input.GetAxis("Horizontal") * speed;
         velocity = transform.position + velocity * Time.deltaTime;
         transform.position = velocity;
+    }
+    public void setSprite(float tempSpeed)
+    {
+        speed = tempSpeed;
+    }
+    public void setSpeed(int sprite)
+    {
+        spriteSelected = sprite;
     }
 }
